0129a25f7888869f73b64ef49d70320ed3c26925
Adam Crow <adamcrow63@gmail.com>
2022-01-12 10:10:04 +1100

    Updated to handle remote linux lan kafka and added script

--- scripts/kafkacat.sh
+++ scripts/kafkacat.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-kafkacat -b alyson.genny.life:9092 -C -o beginning -q -t  kogito-processinstances-events
+#kafkacat -b 192.168.86.37:9092 -C -o beginning -q -t  events 
+kafkacat -b alyson.genny.life:9092 -C -o beginning -q -t  kogito-usertaskinstances-events
+#kafkacat -b alyson.genny.life:9092 -C -o beginning -q -t  kogito-processinstances-events
 
 
