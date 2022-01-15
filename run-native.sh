4b242298de07a3a6f6e1e5f4ad0629f26af1142a
Adam Crow <adamcrow63@gmail.com>
2022-01-15 17:41:39 +1100

    Updated to point to remote alyson.genny.life system

--- run-native.sh
+++ run-native.sh
@@ -3,13 +3,17 @@ export GENNY_SHOW_VALUES=TRUE
 export GENNY_SERVICE_USERNAME=service
 export GENNY_KEYCLOAK_URL=https://keycloak.gada.io
 export GENNY_API_URL=http://internmatch.genny.life:8280
+export GENNY_KAFKA_URL=alyson.genny.life:29092
+export GENNY_CLIENT_ID=kogito-console-quarkus
+export GENNY_REALM=internmatch
+export GENNY_KOGITO_SERVICE_URL=http://alyson2.genny.life:8580
 echo $GENNY_KEYCLOAK_URL
 echo $GENNY_SERVICE_USERNAME
 echo $GENNY_SERVICE_PASSWORD
 echo $GENNY_CLIENT_ID
 echo $GENNY_CLIENT_SECRET
 echo $GENNY_REALM
-export GENNY_INFINISPAN_URL=10.123.123.123:11222
+export GENNY_INFINISPAN_URL=alyson.genny.life:11222
 echo $GENNY_INFINISPAN_URL
 echo $GENNY_INFINISPAN_USERNAME
 echo $GENNY_INFINISPAN_PASSWORD
@@ -18,4 +22,6 @@ echo $GENNY_KOGITO_DATAINDEX_HTTP_URL
 echo $GENNY_KOGITO_DATAINDEX_WS_URL
 echo $GENNY_KOGITO_JOBSERVICE_URL
 echo $GENNY_KOGITO_SERVICE_URL
+
+
 ./target/kogitoq-9.11.0-runner  -Dquarkus.http.port=8580
